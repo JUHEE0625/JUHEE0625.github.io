fa0ef0aad1824575db23d3e4ce5bf7b45c07aeb8
JUHEE0625 <33734894+JUHEE0625@users.noreply.github.com>
2018-04-23 23:20:21 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,334 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Reservation</title>
+<meta name="reservation hotel for malaysia" >
+<meta name="zulkarnain" content="gambohnetwork.com.my">
+<meta name="copyright" content="Hotel Malaysia, inc. Copyright (c) 2014">
+<link rel="stylesheet" href="../scss/normalize.css">
+<link rel="stylesheet" href="../scss/foundation.css">
+<link rel="stylesheet" href="../scss/style.css">
+<link rel="stylesheet" href="../scss/datepicker.css">
+<link href="../scss/datepicker.css" rel="stylesheet" type="text/css"/>
+<link href='http://fonts.googleapis.com/css?family=Slabo+13px' rel='stylesheet' type='text/css'>
+<!--link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/-->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="http://juuheeeee950625.xyz/xmlrpc.php">
+
+<title>juhee &#8211; Just another WordPress site</title>
+<link rel="stylesheet" href="../scss/normalize.css">
+<link rel="stylesheet" href="../scss/foundation.css">
+<link rel="stylesheet" href="../scss/style.css">
+<link rel="stylesheet" href="../scss/datepicker.css">
+<link href="../scss/datepicker.css" rel="stylesheet" type="text/css"/>
+<link href='http://fonts.googleapis.com/css?family=Slabo+13px' rel='stylesheet' type='text/css'>
+<!--link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/-->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+<link rel='dns-prefetch' href='//fonts.googleapis.com' />
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel="alternate" type="application/rss+xml" title="juhee &raquo; Feed" href="http://juuheeeee950625.xyz/feed/" />
+<link rel="alternate" type="application/rss+xml" title="juhee &raquo; Comments Feed" href="http://juuheeeee950625.xyz/comments/feed/" />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.4\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.4\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/juuheeeee950625.xyz\/wp-includes\/js\/wp-emoji-release.min.js"}};
+			!function(a,b,c){function d(a,b){var c=String.fromCharCode;l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,a),0,0);var d=k.toDataURL();l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,b),0,0);var e=k.toDataURL();return d===e}function e(a){var b;if(!l||!l.fillText)return!1;switch(l.textBaseline="top",l.font="600 32px Arial",a){case"flag":return!(b=d([55356,56826,55356,56819],[55356,56826,8203,55356,56819]))&&(b=d([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]),!b);case"emoji":return b=d([55357,56692,8205,9792,65039],[55357,56692,8203,9792,65039]),!b}return!1}function f(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var g,h,i,j,k=b.createElement("canvas"),l=k.getContext&&k.getContext("2d");for(j=Array("flag","emoji"),c.supports={everything:!0,everythingExceptFlag:!0},i=0;i<j.length;i++)c.supports[j[i]]=e(j[i]),c.supports.everything=c.supports.everything&&c.supports[j[i]],"flag"!==j[i]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[j[i]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(h=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",h,!1),a.addEventListener("load",h,!1)):(a.attachEvent("onload",h),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),g=c.source||{},g.concatemoji?f(g.concatemoji):g.wpemoji&&g.twemoji&&(f(g.twemoji),f(g.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link rel='stylesheet' id='sydney-bootstrap-css'  href='http://juuheeeee950625.xyz/wp-content/themes/sydney/css/bootstrap/bootstrap.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='DOPBSP-css-dopselect-css'  href='http://juuheeeee950625.xyz/wp-content/plugins/booking-system/libraries/css/jquery.dop.Select.css' type='text/css' media='all' />
+<link rel='stylesheet' id='sydney-fonts-css'  href='https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400%2C400italic%2C600%7CRaleway%3A400%2C500%2C600' type='text/css' media='all' />
+<link rel='stylesheet' id='sydney-style-css'  href='http://juuheeeee950625.xyz/wp-content/themes/sydney/style.css' type='text/css' media='all' />
+<style id='sydney-style-inline-css' type='text/css'>
+.site-title { font-size:32px; }
+.site-description { font-size:16px; }
+#mainnav ul li a { font-size:14px; }
+h1 { font-size:52px; }
+h2 { font-size:42px; }
+h3 { font-size:32px; }
+h4 { font-size:25px; }
+h5 { font-size:20px; }
+h6 { font-size:18px; }
+body { font-size:16px; }
+.single .hentry .title-post { font-size:36px; }
+.header-image { background-size:cover;}
+.header-image { height:300px; }
+.widget-area .widget_fp_social a,#mainnav ul li a:hover, .sydney_contact_info_widget span, .roll-team .team-content .name,.roll-team .team-item .team-pop .team-social li:hover a,.roll-infomation li.address:before,.roll-infomation li.phone:before,.roll-infomation li.email:before,.roll-testimonials .name,.roll-button.border,.roll-button:hover,.roll-icon-list .icon i,.roll-icon-list .content h3 a:hover,.roll-icon-box.white .content h3 a,.roll-icon-box .icon i,.roll-icon-box .content h3 a:hover,.switcher-container .switcher-icon a:focus,.go-top:hover,.hentry .meta-post a:hover,#mainnav > ul > li > a.active, #mainnav > ul > li > a:hover, button:hover, input[type="button"]:hover, input[type="reset"]:hover, input[type="submit"]:hover, .text-color, .social-menu-widget a, .social-menu-widget a:hover, .archive .team-social li a, a, h1 a, h2 a, h3 a, h4 a, h5 a, h6 a,.classic-alt .meta-post a,.single .hentry .meta-post a { color:#00136b}
+.reply,.woocommerce div.product .woocommerce-tabs ul.tabs li.active,.woocommerce #respond input#submit,.woocommerce a.button,.woocommerce button.button,.woocommerce input.button,.project-filter li a.active, .project-filter li a:hover,.preloader .pre-bounce1, .preloader .pre-bounce2,.roll-team .team-item .team-pop,.roll-progress .progress-animate,.roll-socials li a:hover,.roll-project .project-item .project-pop,.roll-project .project-filter li.active,.roll-project .project-filter li:hover,.roll-button.light:hover,.roll-button.border:hover,.roll-button,.roll-icon-box.white .icon,.owl-theme .owl-controls .owl-page.active span,.owl-theme .owl-controls.clickable .owl-page:hover span,.go-top,.bottom .socials li:hover a,.sidebar .widget:before,.blog-pagination ul li.active,.blog-pagination ul li:hover a,.content-area .hentry:after,.text-slider .maintitle:after,.error-wrap #search-submit:hover,#mainnav .sub-menu li:hover > a,#mainnav ul li ul:after, button, input[type="button"], input[type="reset"], input[type="submit"], .panel-grid-cell .widget-title:after { background-color:#00136b}
+.roll-socials li a:hover,.roll-socials li a,.roll-button.light:hover,.roll-button.border,.roll-button,.roll-icon-list .icon,.roll-icon-box .icon,.owl-theme .owl-controls .owl-page span,.comment .comment-detail,.widget-tags .tag-list a:hover,.blog-pagination ul li,.hentry blockquote,.error-wrap #search-submit:hover,textarea:focus,input[type="text"]:focus,input[type="password"]:focus,input[type="datetime"]:focus,input[type="datetime-local"]:focus,input[type="date"]:focus,input[type="month"]:focus,input[type="time"]:focus,input[type="week"]:focus,input[type="number"]:focus,input[type="email"]:focus,input[type="url"]:focus,input[type="search"]:focus,input[type="tel"]:focus,input[type="color"]:focus, button, input[type="button"], input[type="reset"], input[type="submit"], .archive .team-social li a { border-color:#00136b}
+.site-header.float-header { background-color:rgba(1,20,73,0.9);}
+@media only screen and (max-width: 1024px) { .site-header { background-color:#011449;}}
+.site-title a, .site-title a:hover { color:#ffffff}
+.site-description { color:#ffffff}
+#mainnav ul li a, #mainnav ul li::before { color:#ffffff}
+#mainnav .sub-menu li a { color:#ffffff}
+#mainnav .sub-menu li a { background:#1c1c1c}
+.text-slider .maintitle, .text-slider .subtitle { color:#ffffff}
+body { color:#47425d}
+#secondary { background-color:#ffffff}
+#secondary, #secondary a, #secondary .widget-title { color:#767676}
+.footer-widgets { background-color:#252525}
+.btn-menu { color:#ffffff}
+#mainnav ul li a:hover { color:#3976c6}
+.site-footer { background-color:#1c1c1c}
+.site-footer,.site-footer a { color:#666666}
+.overlay { background-color:#000000}
+.page-wrap { padding-top:83px;}
+.page-wrap { padding-bottom:100px;}
+.slide-inner { display:none;}
+.slide-inner.text-slider-stopped { display:block;}
+@media only screen and (max-width: 1025px) {
+			.mobile-slide {
+				display: block;
+			}
+			.slide-item {
+				background-image: none !important;
+			}
+			.header-slider {
+			}
+			.slide-item {
+				height: auto !important;
+			}
+			.slide-inner {
+				min-height: initial;
+			}
+		}
+@media only screen and (max-width: 780px) {
+    	h1 { font-size: 32px;}
+		h2 { font-size: 28px;}
+		h3 { font-size: 22px;}
+		h4 { font-size: 18px;}
+		h5 { font-size: 16px;}
+		h6 { font-size: 14px;}
+    }
+
+</style>
+<link rel='stylesheet' id='sydney-font-awesome-css'  href='http://juuheeeee950625.xyz/wp-content/themes/sydney/fonts/font-awesome.min.css' type='text/css' media='all' />
+<!--[if lte IE 9]>
+<link rel='stylesheet' id='sydney-ie9-css'  href='http://juuheeeee950625.xyz/wp-content/themes/sydney/css/ie9.css' type='text/css' media='all' />
+<![endif]-->
+<script type='text/javascript' src='http://juuheeeee950625.xyz/wp-includes/js/jquery/jquery.js'></script>
+<script type='text/javascript' src='http://juuheeeee950625.xyz/wp-includes/js/jquery/jquery-migrate.min.js'></script>
+<script type='text/javascript' src='http://juuheeeee950625.xyz/wp-content/plugins/booking-system/libraries/js/dop-prototypes.js'></script>
+<link rel='https://api.w.org/' href='http://juuheeeee950625.xyz/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://juuheeeee950625.xyz/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://juuheeeee950625.xyz/wp-includes/wlwmanifest.xml" />
+<meta name="generator" content="WordPress 4.9.5" />
+<link rel="icon" href="http://juuheeeee950625.xyz/wp-content/uploads/2018/04/cropped-Hilton-logo-1-1-1-32x32.png" sizes="32x32" />
+<link rel="icon" href="http://juuheeeee950625.xyz/wp-content/uploads/2018/04/cropped-Hilton-logo-1-1-1-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="http://juuheeeee950625.xyz/wp-content/uploads/2018/04/cropped-Hilton-logo-1-1-1-180x180.png" />
+<meta name="msapplication-TileImage" content="http://juuheeeee950625.xyz/wp-content/uploads/2018/04/cropped-Hilton-logo-1-1-1-270x270.png" />
+		<style type="text/css" id="wp-custom-css">
+			.page header.entry-header {
+display: none;
+}
+.page .content-wrapper {
+padding-top: 0px !important;
+padding-bottom: 0px !important;
+margin: 0px;
+}		</style>
+	</head>
+<script>
+  $(document).ready(function() {
+		$("#checkout").datepicker();
+		$("#checkin").datepicker({
+		minDate : new Date(),
+		onSelect: function (dateText, inst) {
+        var date = $.datepicker.parseDate($.datepicker._defaults.dateFormat, dateText);
+        $("#checkout").datepicker("option", "minDate", date);
+		}
+		});
+  });
+</script>
+<link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+<meta class="foundation-data-attribute-namespace"><meta class="foundation-mq-xxlarge"><meta class="foundation-mq-xlarge"><meta class="foundation-mq-large"><meta class="foundation-mq-medium"><meta class="foundation-mq-small"><style></style><meta class="foundation-mq-topbar"></head>
+<body class="fontbody" style="background-image : url(img/background.jpg); no-repeat center center fixed; background-size: cover;">
+
+<div class="row foo" style="margin:30px auto 30px auto;"><br><br>
+
+
+</div>
+</div>
+<header id="masthead" class="site-header" role="banner">
+  <div class="header-wrap">
+          <div class="container">
+              <div class="row">
+      <div class="col-md-4 col-sm-8 col-xs-12">
+                    <a href="http://juuheeeee950625.xyz/" title="juhee"><img class="site-logo" src="http://juuheeeee950625.xyz/wp-content/uploads/2018/04/hilton-logo-1.png" alt="juhee" /></a>
+                  </div>
+      <div class="col-md-8 col-sm-4 col-xs-12">
+        <div class="btn-menu"></div>
+        <nav id="mainnav" class="mainnav" role="navigation">
+          <div class="menu-menu-1-container"><ul id="menu-menu-1" class="menu"><li id="menu-item-15" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-15"><a href="../page2/about">ABOUT</a>
+<ul class="sub-menu">
+<li id="menu-item-22" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-22"><a href="http://juuheeeee950625.xyz/history/">history</a></li>
+<li id="menu-item-20" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-20"><a href="http://juuheeeee950625.xyz/qa/">Q&#038;A</a></li>
+</ul>
+</li>
+<li id="menu-item-16" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-16"><a href="http://juuheeeee950625.xyz/offers/">OFFERS</a>
+<ul class="sub-menu">
+<li id="menu-item-18" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-18"><a href="http://juuheeeee950625.xyz/room-2/">Room</a></li>
+<li id="menu-item-17" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-17"><a href="http://juuheeeee950625.xyz/service/">Service</a></li>
+</ul>
+</li>
+<li id="menu-item-90" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-90"><a href="http://juuheeeee950625.xyz/attractions/">ATTRACTIONS</a>
+<ul class="sub-menu">
+<li id="menu-item-86" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-86"><a href="http://juuheeeee950625.xyz/travel-site/">Travel Site</a></li>
+<li id="menu-item-87" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-87"><a href="http://juuheeeee950625.xyz/links/">Links</a></li>
+</ul>
+</li>
+<li id="menu-item-19" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-19"><a href="http://juuheeeee950625.xyz/login/">Login</a>
+<ul class="sub-menu">
+<li id="menu-item-85" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-85"><a href="http://juuheeeee950625.xyz/mypage/">MyPage</a></li>
+</ul>
+</li>
+</ul></div>					</nav><!-- #site-navigation -->
+      </div>
+      </div>
+    </div>
+  </div>
+</header><!-- #masthead -->
+<div class="row">
+	<div class="large-4 columns blackblur fontcolor" style="padding-top:10px;">
+
+	<div class="large-12 columns " >
+	<p><b>Check Date</b></p><hr class="line">
+			<form name="form" action="../page2/checkroom.php" method="post" onSubmit="return validateForm(this);">
+			<div class="row">
+
+					<div class="large-6 columns" style="max-width:100%;">
+						<label class="fontcolor" for="checkin">Check In
+							<input name="checkin" id="checkin" style="width:100%;"/>
+						</label>
+					</div>
+
+					<div class="large-6 columns" style="max-width:100%;">
+						<label class="fontcolor" for="checkout">Check Out
+							<input name="checkout" id="checkout" style="width:100%;"/>
+						</label>
+
+
+					</div>
+			</div>
+
+			<div class="row">
+
+					<div class="large-6 columns">
+						<label class="fontcolor">Adults
+
+								<select  name="totaladults" id="totaladults" style="width:100%;">
+								<option value="0">0</option>
+								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4">4</option>
+								<option value="5">5</option>
+								<option value="6">6</option>
+								<option value="7">7</option>
+								<option value="8">8</option>
+								<option value="9">9</option>
+								<option value="10">10</option>
+								</select>
+
+						</label>
+					</div>
+
+					<div class="large-6 columns"  style="max-width:100%;">
+						<label class="fontcolor">Children
+							<select  name="totalchildrens" id="totalchildrens" style="width:100%; color:black;">
+							<option value="0">0</option>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option value="5">5</option>
+							<option value="6">6</option>
+							<option value="7">7</option>
+							<option value="8">8</option>
+							<option value="9">9</option>
+							<option value="10">10</option>
+							</select>
+						</label>
+					</div>
+
+
+			</div>
+
+			  <div class="row">
+				<div class="large-12 columns" >
+					<button name="submit" href="#" class="button small fontslabo" style="background-color:#2ecc71; width:100%;" >Check Availability</button>
+				</div>
+			  </div>
+			</form>
+	</div>
+
+
+
+</div>
+</div>
+<script>
+	function validateForm(form) {
+		var a = form.checkin.value;
+		var b = form.checkout.value;
+		var c = form.totaladults.value;
+		var d = form.totalchildrens.value;
+			if(a == null || b == null || a == "" || b == "")
+			{
+			 alert("Please choose date");
+			 return false;
+			}
+			if(c == 0)
+			{
+			 	if(d == 0)
+				{
+				 alert("Please choose no. of guest");
+				 return false;
+				}
+			}
+			if(d == 0)
+			{
+			 	if(c == 0)
+				{
+				 alert("Please choose no. of guest");
+				 return false;
+				}
+			}
+
+	}
+</script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57205452-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body></html>
